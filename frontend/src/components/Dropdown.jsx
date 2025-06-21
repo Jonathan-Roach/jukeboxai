61a37b16394bc91f1dda1adbd699781dd0581a08
Jonathan Roach <jonathan.roachy@gmail.com>
2025-06-21 19:56:17 -0400

    re frontend

---
+++ frontend/src/components/Dropdown.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export default function Dropdown() {
+
+    return (
+        <div className="">
+            <form className="">
+                <select className="bg-white py-3 px-2 rounded font-medium outline-none">
+                    <option>Select a Voice</option>
+                    <option>Jamal</option>
+                    <option>Steve</option>
+                    <option>Lady</option>
+                </select>
+            </form>
+        </div>
+    )}
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import NavBar from './NavBar'
-
+import Dropdown from './Dropdown'
 const Hero = () => {
   return (
-    <div className='w-screen h-screen bg-linear-to-br from-orange-900 to-orange-500'>
+    <div className='w-screen h-screen bg-linear-to-br from-orange-900 to-orange-400'>
         <NavBar/>
         <div className='flex flex-col h-2/3 w-full items-center justify-center'>
         <h1 className="w-1/2 text-center text-white font-bold text-6xl my-5" style={{lineHeight:'1.2'}}>Your Music, Your Sound, Made by AI in seconds</h1>
-        <form className='w-1/3'>
+        <div className='w-full flex flex-row items-center justify-center gap-2'>
+        <form className='w-1/2'  action="" method="post">
             <input placeholder="🎙️ Type your music idea here..." type="text" className='bg-gray-900 opacity-70 text-lg text-white outline-none border-gray-700 border-[1px] rounded-xl my-5 w-full h-auto p-4'/>
         </form>
+        <Dropdown/>
         </div>
+    </div>
     </div>
   )
 }
